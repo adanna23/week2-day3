8abc2b50aa7b62dd2dd60a0678b310e4c7dbd0e2
Adanna <aydeeleje@gmail.com>
2021-04-20 13:06:25 -0400

    first commit

---
+++ week2-day3assignment.py
@@ -0,0 +1,18 @@
+l = float(input('Please Enter the Lenght of a Rectangle: '))
+w = float(input('Please Enter the Width of a Rectangle: '))
+ 
+# calculate the area
+Area = l * w
+print("\n Area of a Rectangle is: %.2f" %Area)
+
+
+# # circumference of a circle
+# PI = 3.14
+# radius = float(input(' Please Enter the radius of a circle: '))
+# circumference = 2 * PI * radius
+# print(" Circumference Of a Circle = %.2f" %circumference)
+
+
+
+
+
